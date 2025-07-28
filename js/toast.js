e4cc0c474250017ea1e43eb8d7781afc8aa53268
Jajoo2 <jajoojajoo@tutanota.com>
2025-07-27 23:46:46 -0500

    meow meow! made toasts better + added a toast input to Home

--- js/toast.js
+++ js/toast.js
@@ -1,26 +1,24 @@
 function toastnotify(ntext) {
     const div = document.createElement("div");
-    div.id = "toast";
+    div.className = "toast";
     div.innerHTML = `<p>${ntext}</p>`;
 
-    document.getElementById("toast-container").appendChild(div);
+    const container = document.getElementById("toast-container");
+    container.appendChild(div);
 
-    // First rAF to ensure element is in DOM with initial styles
+    // Ensure transition applies
     requestAnimationFrame(() => {
-        // Second rAF to apply final styles and trigger transition
         requestAnimationFrame(() => {
-            div.style.marginTop = "10px";
+            div.style.transform = "translateY(0)";
             div.style.opacity = "1";
         });
     });
 
-
     setTimeout(() => {
         div.style.opacity = "0";
-
+        div.style.transform = "scale(0.9)";
         div.addEventListener("transitionend", () => {
             div.remove();
         }, { once: true });
     }, 1500);
-
 }
