94c2560dbab5eac025b2bf9b4bc7b851f523a5fa
Jajoo2 <jajoojajoo@tutanota.com>
2025-08-30 23:31:53 -0500

    added a local comment thing

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ link.rel = "icon"
 link.type = "image/x-icon"
 link.href = "img/mei.png"
 document.head.appendChild(link);
+
+function replaceEmojiCodes(text) {
+    return text.replace(/:([a-zA-Z0-9_-]+):/g, (match, filename) => {
+        return `<span style="display:inline-flex; align-items:center; vertical-align:middle;">
+            <img src="img/emoji/${filename}.gif" alt="${filename}" style="height:1.5em; width:auto;"/>
+          </span>`;
+    });
+}
